8cc5ac74ebbf5f8c2400fdf8258e12f77f9e6047
Unknown <jlbartle@hotmail.com>
2018-04-24 22:49:29 -0400

    Update with latest stugg
    error tolerant. have table module, add col module, and options module

--- DB_Proj2_Group3/src/AddColumn.java
+++ DB_Proj2_Group3/src/AddColumn.java
@@ -1,21 +1,48 @@
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
-public class AddColumn {
+public class AddColumn implements ActionListener, WindowListener{
 	
-	JFrame frame;
+	private JFrame frame;
+	private static AddColumn addCol = null;
 	
-	public void closeOptions()
+	public static AddColumn createAddColumnModule()
 	{
-		frame.dispose();
+		if(AddColumn.addCol == null)	
+			AddColumn.addCol = new AddColumn();
+		return AddColumn.addCol;
 	}
 	
+	public static  AddColumn getAddColumnModule()
+	{
+		return AddColumn.addCol;
+	}
+	
+	private AddColumn(){
+		
+	}
+	
+	public void closeWindow()
+	{
+		frame.dispose();
+		this.frame = null;
+		AddColumn.addCol = null;
+	}
+	
+	public JFrame getFrame()
+	{
+		return frame;
+	}
 	
 	public void openWindow()
 	{
@@ -25,22 +52,80 @@ public class AddColumn {
 	
 	private void createFrame()
 	{
-		frame = new JFrame("OPTIONS");
-		frame.setSize(500, 500);
+		frame = new JFrame("ADD COLUMN");
+		frame.setSize(300, 100);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.add(pane);
+		frame.addWindowListener(this);
 		frame.setVisible(true);
-		frame.setSize(200, 600);
-		frame.setLocation(500, 200);
+		frame.setLocation(600, 210);
 	}
 	
-	JPanel pane = new JPanel(new GridLayout(6,1));
-	JButton button[] = new JButton[6];
+	JTextField colName = new JTextField("COLUMN_NAME");
+	JTextField DefaultValue = new JTextField("DEFAULT_VALUE");
+	JTextField DataType = new JTextField("Data_Type");
+	JPanel pane = new JPanel(new GridLayout(2,2));
+	JButton button = new JButton("SUBMIT");
 	
 	private void createLayout()
 	{
+		pane.add(this.colName);
+		pane.add(this.DataType);
+		pane.add(this.DefaultValue);
+		pane.add(this.button);
 		
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == button)
+		{
+			Table.getTable().addColumn(colName.getText());
+			
+			if(this.getFrame() != null)
+				this.closeWindow();
+		}
+		
+	}
+	
+	private boolean checkTextFields()
+	{
+		
+		return true;
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		if(e.getWindow() == frame)
+		{
+			AddColumn.addCol = null;
+		}
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {	
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
 	}
 }
